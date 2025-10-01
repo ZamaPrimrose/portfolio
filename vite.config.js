ea33cb1395378a5de80c90340fdb6b733929dd62
ZamaPrimrose <youremail@example.com>
2025-10-01 09:56:15 +0200

    Fix: Add base path and update workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Make sure this line is there!
+  base: '/portfolio/', // ADD THIS LINE - CRITICAL!
   css: {
     modules: {
       localsConvention: "camelCase",
