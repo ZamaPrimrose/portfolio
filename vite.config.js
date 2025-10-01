c41d2c4278ceb5fcf5cf8dc926b644980799fe35
ZamaPrimrose <youremail@example.com>
2025-10-01 09:50:33 +0200

    Fix GitHub Actions workflow and add base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/portfolio/', // Make sure this line is there!
   css: {
     modules: {
       localsConvention: "camelCase",
